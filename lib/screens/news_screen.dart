24850a929ec57357db4b28315766332367f0d099
Arohi23 <arohirathore786@gmail.com>
2022-06-29 23:57:13 +0530

    added more news ..

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -270,6 +270,82 @@ class _NewsScreenState extends State<NewsScreen> {
                         Text("your text")   
                       ],
                     ),
+                    Row(
+                      children: [
+                       Container(
+                        width:10,
+                       ),
+                       Container(
+                         width:100,
+                         height: 100,
+                         padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
+                         decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          fit: BoxFit.cover,
+                          )
+                        )
+                        ),
+                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
+                        Text("your text")   
+                      ],
+                    ),
+                    Row(
+                      children: [
+                       Container(
+                        width:10,
+                       ),
+                       Container(
+                         width:100,
+                         height: 100,
+                         padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
+                         decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          fit: BoxFit.cover,
+                          )
+                        )
+                        ),
+                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
+                        Text("your text")   
+                      ],
+                    ),
+                    Row(
+                      children: [
+                       Container(
+                        width:10,
+                       ),
+                       Container(
+                         width:100,
+                         height: 100,
+                         padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
+                         decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          fit: BoxFit.cover,
+                          )
+                        )
+                        ),
+                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
+                        Text("your text")   
+                      ],
+                    ),
+                    Row(
+                      children: [
+                       Container(
+                        width:10,
+                       ),
+                       Container(
+                         width:100,
+                         height: 100,
+                         padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
+                         decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          fit: BoxFit.cover,
+                          )
+                        )
+                        ),
+                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
+                        Text("your text")   
+                      ],
+                    ),
                   ]
                   ),
               )
