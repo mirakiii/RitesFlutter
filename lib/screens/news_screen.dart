2c99c7229b0debffd0568f8b310f39608cf4f403
Arohi23 <arohirathore786@gmail.com>
2022-06-30 14:32:57 +0530

    updated news screen with 5 additional columns

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -227,125 +227,188 @@ class _NewsScreenState extends State<NewsScreen> {
 
                 children: [
                   Container(
-                    height: 200,
+                    height: 110,
                     width: double.infinity,
                     // color: Colors.red,
                     // child: Image(image: AssetImage("search.png"))
+                    padding: EdgeInsets.only(right: 10, top: 23, left: 10, bottom: 10),
                     child: Row(
                       children: [
                        Container(
-                        width:10,
-                       ),
-                       Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://www.careerindia.com/img/2021/03/rites-1615443908.jpg"),
+                          image: DecorationImage(image: NetworkImage("https://upload.wikimedia.org/wikipedia/en/thumb/2/23/Container_Corporation_of_India_logo.svg/180px-Container_Corporation_of_India_logo.svg.png"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10), ),
+                        // Text("Railway consultancy firm RITES Ltd on Thursday said it has bagged a contract worth Rs 364.56 crore from Container Corporation of India Ltd.")   
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text ("RITES Ltd on Thursday has bagged a contract worth Rs 364.56 crore from Container Corporation of India Ltd.",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
                   ),
-                  Row(
+
+                  //2nd news column
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    // color: Colors.red,
+                    // child: Image(image: AssetImage("search.png"))
+                    padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                    child: Row(
                       children: [
-                       Container(
-                        width:10,
-                       ),
                        Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          image: DecorationImage(image: NetworkImage("https://www.metrorailnews.in/wp-content/uploads/2022/06/RITES-Guwahati-office-inauguration-696x464.jpeg"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10), ),  
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text (" RITES inaugurated its first office in Guwahati, Assam. Located opposite the Secretariat, in the heart of the city.",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
-                    Row(
+                  ),
+
+                  //3rd column
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    // color: Colors.red,
+                    // child: Image(image: AssetImage("search.png"))
+                    padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                    child: Row(
                       children: [
-                       Container(
-                        width:10,
-                       ),
                        Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          image: DecorationImage(image: NetworkImage("https://th.bing.com/th/id/OIP.yMMT30rrbACCrEBMhOepAwHaEJ?pid=ImgDet&w=1280&h=718&rs=1"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10), ),  
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text ("IIT Guwahati signs MoU with RITES to collaborate for improving infrastructure facilities and promoting multimodal transport system.",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
-                    Row(
+                  ),
+
+                  //4th column
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    // color: Colors.red,
+                    // child: Image(image: AssetImage("search.png"))
+                    padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                    child: Row(
                       children: [
-                       Container(
-                        width:10,
-                       ),
                        Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          image: DecorationImage(image: NetworkImage("https://th.bing.com/th/id/OIP.TkOzptPrUaYFm5uv5pPOygHaEK?pid=ImgDet&rs=1"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10), ),  
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text ("RITES signed a MoU with Grands Trains DU Senegal (GTS-S.A), a railroad company of Senegal, on technical cooperation in the railway sector.",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
-                    Row(
+                  ),
+
+                   //5th column
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                    child: Row(
                       children: [
-                       Container(
-                        width:10,
-                       ),
                        Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          image: DecorationImage(image: NetworkImage("https://www.constructionworld.in/assets/uploads/8f504175cdc2a435d97a91902e4bae54.png"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10, top: 10), ),  
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text ("RITES has been conferred the first position at the 17th National Awards for Excellence in Cost Management-2019.",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
-                    Row(
+                  ),
+
+                   //5th column
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                    child: Row(
                       children: [
-                       Container(
-                        width:10,
-                       ),
                        Container(
                          width:100,
                          height: 100,
                          padding: EdgeInsets.only(right: 10, top: 10, left: 100, bottom: 10),
                          decoration: BoxDecoration(
-                          image: DecorationImage(image: NetworkImage("https://images.unsplash.com/photo-1539269071019-8bc6d57b0205?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                          image: DecorationImage(image: NetworkImage("https://th.bing.com/th/id/OIP.x1cpGnLqHSalbbNkI5EbpgHaE-?pid=ImgDet&rs=1"),
                           fit: BoxFit.cover,
                           )
                         )
                         ),
-                        Padding(padding: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10), ),
-                        Text("your text")   
+                        Padding(padding: EdgeInsets.only(right: 10, left: 10, top: 10), ),  
+                      Container(
+                        width: 220,
+                        height: 80,
+                        child: Text ("RITES signed a MoU with the Planning & Programme Implementation Department, Government of Mizoram",
+                        maxLines: 5,
+                        overflow: TextOverflow.ellipsis),
+                      ),
                       ],
                     ),
+                  ),
+
+
                   ]
                   ),
               )
@@ -524,7 +587,7 @@ class _NewsScreenState extends State<NewsScreen> {
           //     // )
           //   ],
           // ),
-        ));
+        ); 
   }
 }
 
